7f641f587d7eefa337b6fcbef7b85d96cb9ff880
ClintonWeeYuan <clinton_wee@hotmail.com>
2022-07-03 14:22:51 +0800

    Made header

---
+++ components/Header.tsx
@@ -0,0 +1,19 @@
+import {Box, Container, Button, Text, Link} from "@chakra-ui/react"
+import Image from "next/image";
+
+const Header = () => {
+  return(
+    <Container maxW="4xl" sx={{display:"flex", alignItems:"center", justifyContent:"space-between"}}>
+      <Box sx={{display: "flex"}}>
+        <Image src="/skyscanner-logo.png" width="150" height="80"/>
+      </Box>
+      <Box sx={{display: "flex", alignItems:"center", justifyContent:"space-between", width:"50%"}}>
+        <Link>Help</Link>
+        <Button size="sm" variant="outline">English (UK) Malaysia RM MYR</Button>
+        <Button colorScheme="twitter" size="lg" variant="outline">Log In</Button>
+      </Box>
+    </Container>
+  )
+}
+
+export default Header;
